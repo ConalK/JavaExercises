87f8030f1e7c7d1def7dbc54a902b02bf6c1983a
Conal <conalking@gmail.com>
2015-12-16 23:21:08 +0000

    Initial commit

---
+++ src/ReverseString.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Conal on 16/12/2015.
+ */
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        String hi = "Hello";
+        String reverse = new StringBuilder(hi).reverse().toString();
+        System.out.println(reverse);
+    }
+
+}
